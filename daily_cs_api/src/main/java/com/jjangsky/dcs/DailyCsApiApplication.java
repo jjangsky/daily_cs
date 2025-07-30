4922ecdac3e1f20b07c01bd0829efc7af698a7cd
dbcks4646 <dbcks4646@gmail.com>
2025-07-30 15:32:47 +0900

    init: api 서버 프로젝트 생성

---
+++ daily_cs_api/src/main/java/com/jjangsky/dcs/DailyCsApiApplication.java
@@ -0,0 +1,13 @@
+package com.jjangsky.dcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyCsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyCsApiApplication.class, args);
+    }
+
+}
---
+++ daily_cs_api/src/test/java/com/jjangsky/dcs/DailyCsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjangsky.dcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyCsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
